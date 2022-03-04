cae721a9719d12b5896e12364e76151883cb602d
Yash Vaish <yashvaish2021@gmail.com>
2022-03-04 21:55:25 +0530

    Initial Java Commit

---
+++ first.java
@@ -0,0 +1,14 @@
+
+class sum{
+      public static void main(String[] args){
+          int x = 2;
+          int y = 5;
+          System.out.println(x+" "+y);
+          x=x+y;
+          y=x-y;
+          x=x-y;
+          System.out.println(x+" "+y);
+
+      }
+
+}
---
+++ second.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class InputNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x ;
+        System.out.println("Enter any random number");
+        x=sc.nextInt();
+        System.out.println(x);
+       
+
+    }
+}
